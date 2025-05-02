d262e123f52dc09971f27f36d938379d4239c6fe
Alireza parmar <Alirezaparmar@gmail.com>
2025-05-02 13:57:15 +0330

    socket.io

--- public/main.js
+++ public/main.js
@@ -1,32 +1,92 @@
 const socket = io()
-const clientsTotal = document.getElementById('clients-total');
-const messageContainer = document.getElementById('message-conainer');
-const nameInput = document.getElementById('message-input');
-const messageForm = document.getElementById('message-form');
-const messageInput = document.getElementById('message-input');
+const clientsTotal = document.getElementById('clients-total')
+const messageContainer = document.getElementById('message-container') 
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
 
-messageForm.addEventListener('submit', (e) => {
-e.preventDefault()
-sendMessage()
+const sendMessageAudio = new Audio('/1313.mp3')
+
+messageForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    sendMessage()
 })
 
-socket.on('client-total' , (data) => {
- console.log(data)
- clientsTotal.innerText = `total clients : ${data}`
+socket.on('client-total' , (data)=>{
+    console.log(data)
+    clientsTotal.innerText = `total clients : ${data}`
 })
 
-
-function sendMessage() {
-    console.log(messageInput.value)
-    const data = {
+function sendMessage(){
+    if(messageInput.value === '') return
+    const data ={
         name:nameInput.value,
-        message : messageInput.value,
-        dateTime : new Date(),
-
+        message : messageInput.value , 
+        dateTime : new Date()
     }
-    socket.emit('message', data)
+    socket.emit('message',data)
+
+    addMessageToUI(true , data)
 }
 
-socket.on('chat-message', (data) => {
-    console.log(`this data come from server: ${data}`);
-});
+socket.on('chat-message',(data)=>{
+    console.log(data.message)
+    sendMessageAudio.play()
+
+    addMessageToUI(false , data)
+})
+function addMessageToUI(isOwnMessage,data){
+    clearFeedback()
+    const element = 
+    `
+        <li class="${isOwnMessage ? "message-right" : "message-left" } ">
+            <p class="message">
+               ${data.message}
+            </p>
+            <span>${data.name} . ${moment(data.dateTime).fromNow()}</span>
+        </li>
+    `
+    messageContainer.innerHTML += element
+    messageInput.value = ''
+    scrollToBottom()
+}
+
+function scrollToBottom(){
+    messageContainer.scrollTo(0 , messageContainer.scrollHeight)
+}
+
+messageInput.addEventListener('focus',()=>{
+    socket.emit('feedback',{
+        feedback: `✍️ ${nameInput.value} is typing...`
+    })
+})
+messageInput.addEventListener('keypress',()=>{
+    socket.emit('feedback',{
+        feedback: `✍️ ${nameInput.value} is typing...`
+    })
+})
+messageInput.addEventListener('blur',()=>{
+    socket.emit('feedback',{
+        feedback: ``
+    })
+    
+})
+socket.on('feedback',(data)=>{
+    clearFeedback()
+    const element = `
+        <li class="message-feedback" id="message-feedback" >
+            <p class="feedback">
+                ${data.feedback}
+            </p>
+        </li>
+    `
+    messageContainer.innerHTML += element
+    scrollToBottom()
+
+})
+
+function clearFeedback(){
+    document.querySelectorAll('li.message-feedback').forEach(element=>{
+        element.parentNode.removeChild(element)
+    })
+}
